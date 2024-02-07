49be16b4f80fbbef8b857ce74ce6702ce7eb9bd7
AXXISLABS <info@axxis-labs.com>
2024-02-07 20:46:33 +0000

    fyifyi

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import {
     bootstrapCameraKit,
     createMediaStreamSource,
     Transform2D,
-} from '@snap/camera-kit'
+} from '@snap/camera-kit';
 
 (async function(){
     var cameraKit = await bootstrapCameraKit({ apiTocken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzA3MzMzMTAzLCJzdWIiOiJhZjhiMzcyNS0xZmQwLTQxMTUtYWY5Zi0wNTc0MGI2ZTAwY2N-U1RBR0lOR35jNTMxMzA5OC01NTdjLTQ1N2UtYjQ0Mi0zMmYxOGY1ZjM0YmEifQ.GhvD7ay5uIwOGg0yDltIw8_YNe-mA4O9Xh2GTrzbzc4' })
